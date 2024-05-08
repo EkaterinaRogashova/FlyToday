c955c5cda0e1b37b68b82c2dc7f23519bb7d5226
EkaterinaR <rogashovae@mail.ru>
2024-05-08 21:36:29 +0400

    class1

--- FlyToday/Class1.cs
+++ FlyToday/Class1.cs
@@ -2,6 +2,7 @@
 {
     public class Class1
     {
+        public int d = 8;
         public string hi = "Hello";
     }
 }
---
+++ FlyTodayDatabaseImplements/FlyTodayDatabase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyTodayDatabaseImplements
+{
+    public class FlyTodayDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-50HHOEG\SQLEXPRESS;Initial Catalog=FlyTodayDatabaseFull;Integrated Security=True;MultipleActiveResultSets=True;;TrustServerCertificate=True");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
