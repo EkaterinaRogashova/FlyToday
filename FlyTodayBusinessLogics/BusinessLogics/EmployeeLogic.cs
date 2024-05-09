628079c3fd65a443463fa7c21c771ff09ea2ea9d
EkaterinaR <rogashovae@mail.ru>
2024-05-09 16:15:49 +0400

    Классы доб

---
+++ FlyTodayBusinessLogics/BusinessLogics/EmployeeLogic.cs
@@ -0,0 +1,13 @@
+﻿using FlyTodayContracts.BusinessLogicContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyTodayBusinessLogics.BusinessLogics
+{
+    public class EmployeeLogic : IEmployeeLogic
+    {
+    }
+}
