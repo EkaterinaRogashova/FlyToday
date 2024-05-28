79d4bba3310f690b44fe4fb815676cbe85c772f7
Татьяна Артамонова <tania.art03@gmail.com>
2024-05-28 13:23:40 +0400

    p

---
+++ FlyTodayDatabaseImplements/Models/Place.cs
@@ -0,0 +1,20 @@
+﻿using FlyTodayDataModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyTodayDatabaseImplements.Models
+{
+    public class Place : IPlaceModel
+    {
+        public int FlightId => throw new NotImplementedException();
+
+        public bool IsFree => throw new NotImplementedException();
+
+        public int Id => throw new NotImplementedException();
+
+        string IPlaceModel.Place => throw new NotImplementedException();
+    }
+}
