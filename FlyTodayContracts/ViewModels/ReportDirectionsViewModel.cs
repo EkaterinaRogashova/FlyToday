c9548465344cf6446f8577a9e4265c47ae36208a
Татьяна Артамонова <tania.art03@gmail.com>
2024-06-12 21:05:33 +0400

    начало делания отчетов

---
+++ FlyTodayContracts/ViewModels/ReportDirectionsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FlyTodayContracts.ViewModels
+{
+    public class ReportDirectionsViewModel
+    {
+        public int Id { get; set; }
+        public int Direction { get; set; }
+        public string Ticket { get; set; } = string.Empty;
+
+    }
+}
